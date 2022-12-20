cc856844c592f175ec4827e3abc2f28a9b3f0c93
roriloty <roriloty@gmail.com>
2022-12-20 18:53:34 +0700

    add_script

---
+++ PPNITY chiangrai/Assets/Script/MainVarHolder.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainVarHolder : MonoBehaviour
+{
+    public static int Energy = 0;
+    public static int MaxEnergy = 0;
+    public static int Money = 0;
+}
